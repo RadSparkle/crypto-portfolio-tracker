51eee88c241e81d261b1773a524c855308eac10f
RadSparkle <wwwartg1@naver.com>
2025-04-07 01:16:05 +0900

    Initial commit

---
+++ src/main/java/io/github/radsparkle/cryptoportfoliotracker/CrptyoPortfolioTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.github.radsparkle.cryptoportfoliotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrptyoPortfolioTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrptyoPortfolioTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/radsparkle/cryptoportfoliotracker/CrptyoPortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.radsparkle.cryptoportfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrptyoPortfolioTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
